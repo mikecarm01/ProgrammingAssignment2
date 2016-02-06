b04fe4354d117b072ba0d53fd967f9de2f938b63
mikecarm01 <mikecarm01@gmail.com>
2016-02-06 23:04:54 +1100

    Create functions for calcuating matrix inverse

---
+++ Crazy.R
@@ -0,0 +1,8 @@
+crazy <- function(){
+  x <<- 3.14
+  print(x)
+  print(x)
+  x <- 42
+  print(x)
+  print(x)
+}
--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Below are two functions that are used to create a special object
+# that stores an invertible matrix and cache's its inverse.
 
-## Write a short comment describing this function
+# The first function, makeCacheMatrix creates a special "matrix", which is really a
+# list containing a function to
+
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the value of the inverse
+# 4. Get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
+# This function tests the value of the stored inverse and if it is NOT NULL
+# returns the cached value of the previously calculated matrix inverse.
+# If the stored inverse is the initial NULL value, it will use the solve()
+# function calculate the inverse and the set function from above to cache it,
+# then display the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
